981b9027739fc4b6223ea701d3801c7ba5a2a043
Nikita Ulyantsev <yan.xhrome@gmail.com>
2022-08-21 15:28:51 +0300

    Take notes from db.

--- src/main/java/me/itanik/todo/backend/controllers/NoteController.java
+++ src/main/java/me/itanik/todo/backend/controllers/NoteController.java
@@ -1,18 +1,28 @@
 package me.itanik.todo.backend.controllers;
 
-import me.itanik.todo.backend.model.dto.TodoNote;
+import me.itanik.todo.backend.model.entity.NoteEntity;
+import me.itanik.todo.backend.model.repository.NoteRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class NoteController {
 
+    private final NoteRepository noteRepository;
+
+    public NoteController(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
+
     @GetMapping("/note")
-    public List<TodoNote> getNote(@RequestParam(value = "userId") String userId) {
-        return new ArrayList<>();
+    public Iterable<NoteEntity> getNotes(@RequestParam(value = "userId") String userId) {
+        final List<UUID> ids = new ArrayList<>();
+        ids.add(UUID.fromString(userId));
+        return noteRepository.findAllById(ids);
     }
 }
